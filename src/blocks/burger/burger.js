f427c287e6ccf625d5a21674ede453f8edde2a90
Viacheslav <zvv56739@gmail.com>
2021-10-06 21:30:05 +0300

    added menu new

--- src/blocks/burger/burger.js
+++ src/blocks/burger/burger.js
@@ -1,5 +1,5 @@
 $('.burger').click(function(){
     $(this).parents('.root').css({"overflow": "hidden"});
-    $('<div class="overlay"></div>').hide().prependTo('.root').fadeIn();
+    $('<div class="overlay"></div>').hide().prependTo('.root').fadeIn(250);
     $(this).nextAll('.menu').animate({width:'show'}, 250);
 })
--- src/blocks/menu/menu.js
+++ src/blocks/menu/menu.js
@@ -1,40 +1,51 @@
-// CLOSE
-$('.menu__close').on('click', function(){
-    var $root = $(this).parents('.root');
-    var $overlay = $root.children('.overlay')
-    var $menu = $(this).parents('.menu');
-    $root.removeAttr("style");
-    $overlay.fadeOut();
-        setTimeout(function(){
-            $overlay.remove();
-        },350);
-        if($menu.is(':visible')){
-            $menu.animate({width:'hide'}, 250);
-        }else{
-            $root.removeAttr("style");
-            $(this).off('click');
+$(document).ready(function(){
+    // CLOSE
+    $('.menu__close').on('click', function(){
+        const $root = $(this).parents('.root');
+        const $overlay = $('.overlay');
+        const $menu = $(this).parents('.menu');
+
+        $($root).removeAttr("style");
+        $overlay.fadeOut(250);
+            setTimeout(function(){
+                $overlay.remove();
+            },250);
+        $menu.animate({width:'hide'}, 250);
+    })
+
+    // SUBMENU
+    $('.menu__title').on('click',function(){
+        const $submenu = $(this).children('.menu__submenu');
+        const $menu = $(this).parents('.menu');
+        console.log($menu.height())
+
+        if( $submenu.is(':hidden')){
+            $(this).children('.menu__submenu').animate({width:'show',}, 250);
+            $('<div class="overlay"></div>').css('opacity', '0.6').hide().prependTo($menu).fadeIn(350);
         }
-})
+    })
 
-// SUBMENU
-$('.menu__title').click(function(){
-    $(this).children('.menu__submenu').animate({width:'show'}, 250);
-})
+    // SUB-SUBMENU
+    $('.menu__item').click(function(e){
+        $(this).children('.menu__sub-submenu').animate({height:'toggle'}, 250);
+        e.stopPropagation();
+    })
 
-// SUB-SUBMENU
-$('.menu__item').click(function(e){
-    $(this).children('.menu__sub-submenu').animate({height:'toggle'}, 650);
-    e.stopPropagation();
-})
+    // BACK
+    $('.menu__back').click(function(e){
+        var $sub_submenu = $(this).next('.menu__sublist').find('.menu__sub-submenu');
+        var $submenu = $(this).parent('.menu__submenu');
+        var $overlay = $(this).parents('.menu').children('.overlay');
 
-// BACK
-$('.menu__back').click(function(e){
-    var $sub_submenu = $(this).next('.menu__sublist').find('.menu__sub-submenu');
-    var $submenu = $(this).parent('.menu__submenu');
-    if ($sub_submenu.is(':visible') && $submenu.is(':visible')) {
-        $sub_submenu.animate({height:'hide'}, 250)
-    }else{
-        $submenu.animate({width:'hide'}, 250)            
-    }
-    e.stopPropagation();
+            if ($sub_submenu.is(':visible') && $submenu.is(':visible')) {
+                $sub_submenu.animate({height:'hide'}, 250)
+            }else{
+                $submenu.animate({width:'hide'}, 250)
+                $overlay.fadeOut(250);
+                setTimeout(function(){
+                    $overlay.remove();
+                }, 250); 
+            }
+        e.stopPropagation();
+    })
 })
